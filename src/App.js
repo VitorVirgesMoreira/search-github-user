f1d7b3eb67ca6ec69d2129e31b2ffacab819a0c6
Vitor Virges <vitor.virges@gmail.com>
2022-04-21 17:47:50 -0300

    feat: light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import "./App.css";
+import { useState } from "react";
+import { getUser } from "./services/githubService";
 import {
   SearchOutlined,
   EnvironmentOutlined,
   LinkOutlined,
   TwitterOutlined,
 } from "@ant-design/icons";
-import { ReactComponent as CompanyLogo } from "./assets/c1d1ccb5568f90d979c1aaac2977a2c2.svg";
-import { getUser } from "./services/githubService";
-import { useState } from "react";
-import moment from "moment";
+import { ReactComponent as CompanyLogo } from "./assets/company.svg";
+import { ReactComponent as SunIcon } from "./assets/sun.svg";
+import { ReactComponent as MoonIcon } from "./assets/moon.svg";
 
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes";
+import moment from "moment";
 import "moment/locale/pt-br";
+import "./App.css";
+
+const StyledApp = styled.div``;
+
+moment.locale("pt-br");
+
 function App() {
-  moment.locale("pt-br");
+  const [theme, setTheme] = useState("light");
+  const [username, setUsername] = useState("");
   const [response, setResponse] = useState({
     avatar_url: "",
     name: "",
@@ -28,7 +38,10 @@ function App() {
     blog: "",
     company: "",
   });
-  const [username, setUsername] = useState("");
+
+  const themeToogler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   async function handleClick() {
     const user = await getUser(username);
@@ -36,129 +49,141 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="Container">
-        <header className="App-header">
-          <p>Git finder</p>
-        </header>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp className="App">
+        <div className="Container">
+          <header className="App-header">
+            <p>Git finder</p>
 
-        <div className="Search">
-          <SearchOutlined className="search-icon" />
-          <input
-            className="input-search-text"
-            type="text"
-            placeholder="Digite o username do GitHub"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <button
-            className="search-button"
-            onClick={handleClick}
-          >
-            Pesquisar
-          </button>
-        </div>
+            {theme === "light" ? (
+              <button className="changeTheme" onClick={() => themeToogler()}>
+                Light Mode <SunIcon />
+              </button>
+            ) : (
+              <button className="changeTheme" onClick={() => themeToogler()}>
+                Dark Mode <MoonIcon />
+              </button>
+            )}
+          </header>
 
-        <div className="Content">
-          <div className="user-photo">
-            <img
-              className="photo"
-              src={
-                response.avatar_url
-                  ? response.avatar_url
-                  : "https://avatars.githubusercontent.com/u/583231?v=4"
-              }
-              alt="user_photo"
+          <div className="Search">
+            <SearchOutlined className="search-icon" />
+            <input
+              className="input-search-text"
+              type="text"
+              placeholder="Digite o username do GitHub"
+              onChange={(e) => setUsername(e.target.value)}
             />
+            <button className="search-button" onClick={handleClick}>
+              Pesquisar
+            </button>
           </div>
-          <div className="form-group">
-            <div className="user-header">
-              <div className="infos">
-                <label className="name">
-                  {response.name
-                    ? response.name
-                    : response.login || "Nome do usuário"}
-                </label>
-                {response.created_at ? (
-                  <label className="created-at">
-                    Entrou {moment(response.created_at).format("d MMM YYYY")}
+
+          <div className="Content">
+            <div className="user-photo">
+              <img
+                className="photo"
+                src={
+                  response.avatar_url
+                    ? response.avatar_url
+                    : "https://avatars.githubusercontent.com/u/583231?v=4"
+                }
+                alt="user_photo"
+              />
+            </div>
+            <div className="form-group">
+              <div className="user-header">
+                <div className="infos">
+                  <label className="name">
+                    {response.name
+                      ? response.name
+                      : response.login || "Nome do usuário"}
                   </label>
+                  {response.created_at ? (
+                    <label className="created-at">
+                      Entrou {moment(response.created_at).format("d MMM YYYY")}
+                    </label>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                {response.login ? (
+                  <a
+                    className="github-link"
+                    href={response.html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    @{response.login}
+                  </a>
                 ) : (
                   <></>
                 )}
               </div>
-              {response.login ? (
-                <a
-                  className="github-link"
-                  href={response.html_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  @{response.login}
-                </a>
-              ) : (
-                <></>
-              )}
-            </div>
 
-            <div className="user-bio">
-              <p>
-                {response.bio
-                  ? response.bio
-                  : "Esse perfil não possui biografia"}
-              </p>
-            </div>
+              <div className="user-bio">
+                <p>
+                  {response.bio
+                    ? response.bio
+                    : "Esse perfil não possui biografia"}
+                </p>
+              </div>
 
-            <div className="user-counters">
-              <div className="counters">
-                <p>Repos</p>
-                <label>{response.public_repos}</label>
+              <div className="user-counters">
+                <div className="counters">
+                  <p>Repos</p>
+                  <label>
+                    {response.public_repos ? response.public_repos : 0}
+                  </label>
+                </div>
+                <div className="counters">
+                  <p>Seguidores</p>
+                  <label>{response.followers ? response.followers : 0}</label>
+                </div>
+                <div className="counters">
+                  <p>Seguindo</p>
+                  <label>{response.following ? response.following : 0}</label>
+                </div>
               </div>
-              <div className="counters">
-                <p>Seguidores</p>
-                <label>{response.followers}</label>
-              </div>
-              <div className="counters">
-                <p>Seguindo</p>
-                <label>{response.following}</label>
-              </div>
-            </div>
 
-            <div className="user-footer">
-              <div className="footer-info">
-                <EnvironmentOutlined />
-                <label>
-                  {response.location ? response.location : "Não disponível"}
-                </label>
-              </div>
-              <div className="footer-info">
-                <TwitterOutlined />
-                {response.twitter_username ? (
-                  <a href={"https://twitter.com/"}>
-                    @{response.twitter_username}
-                  </a>
-                ) : (
-                  <label>Não disponível</label>
-                )}
-              </div>
-              <div className="footer-info">
-                <LinkOutlined />
-                {response.blog ? (
-                  <a href={response.blog}>{response.blog}</a>
-                ) : (
-                  <label>Não disponível</label>
-                )}
-              </div>
-              <div className="footer-info">
-                <CompanyLogo />
-                <label>
-                  {response.company ? response.company : "Não disponível"}
-                </label>
+              <div className="user-footer">
+                <div className="footer-info">
+                  <EnvironmentOutlined />
+                  <label>
+                    {response.location ? response.location : "Não disponível"}
+                  </label>
+                </div>
+                <div className="footer-info">
+                  <TwitterOutlined />
+                  {response.twitter_username ? (
+                    <a href={"https://twitter.com/"}>
+                      @{response.twitter_username}
+                    </a>
+                  ) : (
+                    <label>Não disponível</label>
+                  )}
+                </div>
+                <div className="footer-info">
+                  <LinkOutlined />
+                  {response.blog ? (
+                    <a href={response.blog}>{response.blog}</a>
+                  ) : (
+                    <label>Não disponível</label>
+                  )}
+                </div>
+                <div className="footer-info">
+                  <CompanyLogo />
+                  <label>
+                    {response.company ? response.company : "Não disponível"}
+                  </label>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,87 @@
+import { createGlobalStyle } from "styled-components";
+
+export const lightTheme = {
+  backgroundColor: "#fff",
+  primaryColor: "#f7f9f9",
+  secundaryColor: "#00ba7c",
+
+  fontColor: "#000",
+  inputSearchTextColor: "#536471",
+  bioTextColor: "#536471",
+  countersTextColor: "#536471",
+  footerTextColor: "#536471",
+
+  searchIcon: "#000",
+  contentSvgFill: "#000",
+};
+
+export const darkTheme = {
+  backgroundColor: "#000",
+  primaryColor: "#15202b",
+  secundaryColor: "#00ba7c",
+
+  fontColor: "#FFF",
+  inputSearchTextColor: "#8b98a5",
+  bioTextColor: "#8b98a5",
+  countersTextColor: "#8b98a5",
+  footerTextColor: "#8b98a5",
+
+  searchIcon: "#8b98a5",
+  contentSvgFill: "#FFF",
+};
+
+export const GlobalStyles = createGlobalStyle`
+    body { 
+        background-color: ${(props) => props.theme.backgroundColor};
+    }
+
+    .App-header {
+        background-color: ${(props) => props.theme.backgroundColor};
+        color:  ${(props) => props.theme.fontColor};
+    }
+
+    .App-header .changeTheme {
+        color:  ${(props) => props.theme.fontColor};
+    }
+
+
+    .Search {
+        background-color: ${(props) => props.theme.primaryColor};
+    }
+
+    span.anticon.anticon-search.search-icon {
+        color: ${(props) => props.theme.searchIcon};
+    }
+
+    .Search input[type="text"],
+    .Search .input-search-text::-webkit-input-placeholder {
+        color: ${(props) => props.theme.inputSearchTextColor};
+    }       
+
+    .Content {
+        background-color: ${(props) => props.theme.primaryColor};
+        color:  ${(props) => props.theme.fontColor};
+    }
+
+    .Content svg {
+        fill: ${(props) => props.theme.contentSvgFill};
+    }
+
+    .user-bio {
+        color: ${(props) => props.theme.bioTextColor};
+    }
+
+    .user-counters .counters p {
+        color: ${(props) => props.theme.countersTextColor};
+    }
+
+    .user-footer {
+        color: ${(props) => props.theme.countersTextColor};
+    }
+    
+    a:link,
+    a:visited {
+        color: var(--secundary-color) !important;
+        text-decoration: none;
+    }
+`;
